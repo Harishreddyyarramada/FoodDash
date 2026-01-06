690fd07a32d1c9be46e5c6f1c5092b9b3bb029d6
Harish Reddy Yarramada <129584343+Harishreddyyarramada@users.noreply.github.com>
2026-01-06 22:55:58 +0530

    Delete src/firebase/config.ts

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-4594767536-ed75d",
-  "appId": "1:78816087918:web:c85b8fcd6bdfcb1177bb7f",
-  "apiKey": "AIzaSyAWE2cLtq2bKN77oScsAzagJKKbuF8OnZE",
-  "authDomain": "studio-4594767536-ed75d.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "78816087918"
-};
